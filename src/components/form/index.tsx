3166a4de471592dcd6a604776ce1f122b6ab3943
Gabriel-C-Amaral <gabrielcorreaamaral94@gmail.com>
2023-02-08 02:30:17 -0300

    Changes to be committed: 	modified:   src/components/form/index.tsx

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -3,53 +3,34 @@ import React, { ChangeEvent } from "react";
 
 type State = {
     name: string
-    cep: number | undefined
-    numero:number | undefined
-    complemento: string
-    bairro: string
-    cidade: string
-    estado: string
+    lista: string
 }
 
 class Formulario extends React.Component<object, State> {
     state = {
         name: "",
-    cep: undefined,
-    numero:undefined,
-    complemento: "",
-    bairro: "",
-    cidade: "",
-    estado: "",
+        lista: ''
+    
     }
+     abc: string[] = []
+    addTask = () => {
 
+        this.abc.push(this.state.name)
 
-showInputValue = (event: React.MouseEvent) => {
-    event.preventDefault()
-   
-}
+    }
+  
+
 
     render() {
         return (
         <div >
-        <label htmlFor="adress">Nome da rua: </label>
-        <input type="text" name="adress" id="adress" value={this.state.name} onChange={(event: ChangeEvent<HTMLInputElement>) => this.setState({name: event.target.value})}/>
-        <label htmlFor="numero">Numero:</label>
-        <input type="number" name="numero" id="numero" value={this.state.numero} onChange={(event: ChangeEvent<HTMLInputElement>) => this.setState({numero: Number(event.target.value)})}/>
-        <label htmlFor="cep">CEP:</label>
-        <input type="number" name="cep" id="cep" value={this.state.cep} onChange={(event: ChangeEvent<HTMLInputElement>) => this.setState({cep: Number(event.target.value)})}/>
-        <label htmlFor="complemento">Complemento</label>
-        <input type="text" name="complemento" id="complemento" value={this.state.complemento} onChange={(event: ChangeEvent<HTMLInputElement>) => this.setState({complemento: event.target.value})}/>
-        <label htmlFor="bairro">Bairro</label>
-        <input type="text" name="bairro" id="bairro" value={this.state.bairro} onChange={(event: ChangeEvent<HTMLInputElement>) => this.setState({bairro: event.target.value})}/>
-        <label htmlFor="cidade">Cidade</label>
-        <input type="text" name="cidade" id="cidade" value={this.state.cidade} onChange={(event: ChangeEvent<HTMLInputElement>) => this.setState({cidade: event.target.value})}/>
-        <label htmlFor="estado">Estado</label>
-        <input type="text" name="estado" id="estado" value={this.state.estado} onChange={(event: ChangeEvent<HTMLInputElement>) => this.setState({estado: event.target.value})}/>
-        <button onClick={this.showInputValue}> Enviar</button>
-
-        <div>
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
-        </div>
+            <label htmlFor="task">Digite a tarefa</label>
+            <input type="text" id="task" name="task" value={this.state.name} onChange={(event: ChangeEvent<HTMLInputElement>) => this.setState({name: event.target.value})} />
+            <button onClick={this.addTask}>Mostrar valor</button>
+            <div id="lista">{this.abc.map(function(value: string, key): any{
+            return (<div key={key}>{value}</div>)
+        })}</div>
+                    
       </div>
         )
     }
